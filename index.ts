9169d21bbec6b1c447edd1759aa3281558a4deb1
oscharko83@gmail.com <59687448+oscharko@users.noreply.github.com>
2022-05-27 15:58:25 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-let nums = [0, 1, 0, 3, 12]
+let nums = [1]
 
-const moveZeroesWhithConcat = function(nums: number[]): number[] {
+const moveZeroesWhithConcat = function (nums: number[]): number[] {
   let zeroArray: number[] = []
   if (nums.length > 1 && nums.reduce((a, b) => a + b) != 0 && nums.indexOf(0) != -1) {
     while (nums.indexOf(0) != -1) {
@@ -12,8 +12,8 @@ const moveZeroesWhithConcat = function(nums: number[]): number[] {
   return nums
 }
 
-const moveZeroesWhiteForLoop = function (nums: number[]): number[]{
-  let zeroArray: number[]= []
+const moveZeroesWhiteForLoop = function (nums: number[]): number[] {
+  let zeroArray: number[] = []
   if (nums.length > 1 && nums.reduce((a, b) => a + b) != 0 && nums.indexOf(0) != -1) {
     while (nums.indexOf(0) != -1) {
       nums.splice(nums.indexOf(0), 1)
@@ -27,5 +27,38 @@ const moveZeroesWhiteForLoop = function (nums: number[]): number[]{
     return nums
   }
 }
+
+function moveZeroesFoorLoop(nums: number[]): number[] {
+  let j: number = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      const zeroes = nums[i]
+      nums[i] = 0
+      nums[j++] = zeroes
+    }
+  }
+  return nums
+};
+
+function moveZeroes(nums: number[]): number[] {
+  /*
+  # Description for array v:
+  #   - v[0] = input array and also output rotate array
+  #   - v[1] = increment runner for while loop condition
+  #   - v[2] = increment runner in if condition for index put array output
+  #   - v[3] = memory for elements from array greater zero
+  */
+  let v: [number[], number, number, number] = [nums, 0, 0, 0]
+  while (v[1] < v[0].length) {
+    if (v[0][v[1]] !== 0) {
+      v[3] = v[0][v[1]]
+      v[0][v[1]] = 0
+      v[0][v[2]++] = v[3]
+    }
+    v[1]++
+  }
+  return v[0]
+};
 
 console.log(moveZeroes(nums))
