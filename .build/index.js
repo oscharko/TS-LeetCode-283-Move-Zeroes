7b2b406e5af23a90cb01a6d35d003fe208d67052
Oliver Scharkowski <59687448+oscharko@users.noreply.github.com>
2022-05-27 13:59:35 +0000

    update final solution

--- .build/index.js
+++ .build/index.js
@@ -1,1 +1,29 @@
+let nums = [0, 1, 0, 3, 12];
+const moveZeroes = function(nums2) {
+  let zeroArray = [];
+  if (nums2.length > 1 && nums2.reduce((a, b) => a + b) != 0 && nums2.indexOf(0) != -1) {
+    while (nums2.indexOf(0) != -1) {
+      nums2.splice(nums2.indexOf(0), 1);
+      zeroArray.push(0);
+    }
+    return nums2.concat(zeroArray);
+  }
+  return nums2;
+};
+const moveZeroesWhiteForLoop = function(nums2) {
+  let zeroArray = [];
+  if (nums2.length > 1 && nums2.reduce((a, b) => a + b) != 0 && nums2.indexOf(0) != -1) {
+    while (nums2.indexOf(0) != -1) {
+      nums2.splice(nums2.indexOf(0), 1);
+      zeroArray.push(0);
+    }
+    for (let i = 0; i < zeroArray.length; i++) {
+      nums2.push(0);
+    }
+    return nums2;
+  } else {
+    return nums2;
+  }
+};
+console.log(moveZeroes(nums));
 //# sourceMappingURL=index.js.map
